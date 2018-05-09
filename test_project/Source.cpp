dfa51fc59df888d1ce72fea9d075d60585f017f5
Kent Kurosawa <mots_de_la_magie.s@outlook.jp>
2018-05-09 10:04:40 +0900

    Test project

--- test_project/Source.cpp
+++ test_project/Source.cpp
@@ -1,118 +1,46 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <conio.h>
-#include <string>
 #include <iostream>
-#include <memory>
-#include <list>
+#include <string.h>
 
-class Base {
+#define STR_MAX 31
+#define _CRT_SECURE_NO_WARNINGS
+
+#pragma warning (disable:4996)
+
+class classA {
+public:
+	classA(void) { strcpy(strings, "クラスA"); }
+	~classA(){ }
+
 protected:
-	std::string m_str;
+	char* get(void);
 
-public:
-	Base();
-	~Base();
-	inline Base* GetRef() {
-		std::cout << "GetRef():" << this << std::endl;
-		return this;
-	}
-	inline void DisplayRef() {
-		std::cout << "DisplayRef():" << this << std::endl;
-	}
-};
-
-class Manager {
 private:
-	std::list<Base*> mlis_objects;
-	unsigned char* muc_ptr;
-
-public:
-	Manager();
-	~Manager();
-	inline void AddObject(Base* _object) {
-		mlis_objects.push_back(_object);
-	}
-
-	void Update();
-
-	int Deleter();
+	char strings[STR_MAX + 1];
 };
 
-class Manager g_manager;
+class classB :public classA {
+public:
+	classB(void) { strcpy(strings, "クラスB"); }
+	~classB(){}
+	void disp(char*);
+	void disp(void);
+private:
+	char strings[STR_MAX + 1];
+};
 
-Manager::Manager() {
-
+char* classA::get(void) { return strings; }
+void classB::disp(char* pStr) { std::cout << pStr << std::endl; }
+void classB::disp(void) { std::cout << classA::get() << std::endl;
+std::cout << classB::get() << std::endl;
 }
 
-Manager::~Manager() {
-	std::list<Base*>::iterator iter = mlis_objects.begin();
-	while (iter != mlis_objects.end()) {
-		delete *iter;
-		++iter;
-	}
-}
+int main(void) {
+	classB* pClass;
+	pClass = new classB;
+	pClass->disp();
+	pClass->disp("クラス外");
+	delete pClass;
 
-void Manager::Update() {
-	std::list<Base*>::iterator iter_objects = mlis_objects.begin();
-	while (iter_objects != mlis_objects.end()) {
-		Base* object = *iter_objects;
-		object->DisplayRef();
-		++iter_objects;
-	}
-}
-
-int Manager::Deleter() {
-	std::list<Base*>::iterator iter = mlis_objects.begin();
-	while (iter != mlis_objects.end()){
-		if (!*iter) { return -1; }
-		iter = mlis_objects.erase(iter);
-	}
-
+	getchar();
 	return 0;
-}
-
-
-
-Base::Base(){
-	m_str = "Base():";
-	std::cout << m_str << this << std::endl;
-	g_manager.AddObject(this);
-}
-
-Base::~Base(){ }
-
-class Object :public Base {
-public:
-	Object();
-	~Object();
-};
-
-Object::Object() {
-	m_str = "Object():";
-	std::cout << m_str << this << std::endl;
-}
-
-Object::~Object() {
-
-}
-
-
-
-
-void main() {
-
-	while (1){
-		if ('a' == _getch()) {
-			break;
-		}
-
- 		if (' ' == _getch()) {
-			Object* object = new Object;
-		}
-
-		g_manager.Update();
-	}
-
-	g_manager.Deleter();
 }
